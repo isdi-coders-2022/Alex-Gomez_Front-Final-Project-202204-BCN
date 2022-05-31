13af5c2b0a7b73d25521de68c142c764baa1e3cb
Alejandro Gomez <jandrogo@gmail.com>
2022-05-31 12:06:00 +0200

    installed maaterialUi components and created RegisterForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 function App() {
-  return <div className="App"></div>;
+  return <RegisterForm />;
 }
 
 export default App;
