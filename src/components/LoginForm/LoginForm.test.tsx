600e87c357f5da2e0985f5c2c4cd4d624db7bac7
Alejandro Gomez <jandrogo@gmail.com>
2022-06-01 19:21:04 +0200

    Added test to cover 100% lines in LoginForm component

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
 import LoginForm from "./LoginForm";
@@ -11,10 +12,36 @@ describe("Given a LoginForm component", () => {
           <LoginForm />
         </Provider>
       );
-
       expect(screen.getByLabelText("Username")).toBeInTheDocument();
       expect(screen.getByLabelText("Password")).toBeInTheDocument();
       expect(screen.getByRole("button")).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's instantiated and login button is pressed", () => {
+    test("Then it should call the submitLogin function", () => {
+      render(
+        <Provider store={store}>
+          <LoginForm />
+        </Provider>
+      );
+      const usernameInput = "Username";
+      const username = "Marta";
+      const passwordInput = "Password";
+      const password = "1234";
+
+      const nameText = screen.getByLabelText(usernameInput);
+      userEvent.type(nameText, username);
+      const passwordText = screen.getByLabelText(passwordInput);
+      userEvent.type(passwordText, password);
+
+      expect(nameText).toHaveValue(username);
+      expect(passwordText).toHaveValue(password);
+
+      const button = screen.getByRole("button", { name: /Login/i });
+
+      expect(button).toBeEnabled();
+      userEvent.click(button);
     });
   });
 });
