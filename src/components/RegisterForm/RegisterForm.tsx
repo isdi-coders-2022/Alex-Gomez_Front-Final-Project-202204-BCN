23422da567989cefe833e628ff3d6072a006183e
Alejandro Gomez <jandrogo@gmail.com>
2022-06-01 09:25:15 +0200

    Modified RegisterForm and added a test to to cover 66%

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,20 +1,62 @@
+import { useState } from "react";
 import RegisteredFormStyled from "./RegisterFormStyled";
 
 const RegisterForm = () => {
+  const blankFields = {
+    name: "",
+    username: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(blankFields);
+
+  const changeData = (event: { target: { id: string; value: string } }) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
   const submitRegister = (event: React.SyntheticEvent) => {
     event.preventDefault();
+    setFormData(blankFields);
   };
 
   return (
     <RegisteredFormStyled>
       <form onSubmit={submitRegister} noValidate autoComplete="off">
-        <label htmlFor="nameRegister">Name</label>
-        <input id="name" autoComplete="off" />
-        <label htmlFor="usernameRegister">Username</label>
-        <input id="userName" autoComplete="off" />
-        <label htmlFor="passwordRegister">Password</label>
-        <input id="password" type="password" autoComplete="off" />
-        <button type="submit" value="Send">
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          value={formData.name}
+          onChange={changeData}
+          autoComplete="off"
+        />
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          value={formData.username}
+          onChange={changeData}
+          autoComplete="off"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={formData.password}
+          onChange={changeData}
+          autoComplete="off"
+        />
+        <button
+          type="submit"
+          value="Send"
+          disabled={
+            formData.name === "" ||
+            formData.username === "" ||
+            formData.password === ""
+          }
+          onClick={submitRegister}
+        >
           Register
         </button>
       </form>
