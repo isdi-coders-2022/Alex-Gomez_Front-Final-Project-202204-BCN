9635cb690b9b67ef0a2eae57cf8f32a3b4bf0d72
Alejandro Gomez <jandrogo@gmail.com>
2022-06-01 17:36:05 +0200

    Added userLoginThunk test and modified mocks to make it work

--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -1,10 +1,14 @@
 import { rest } from "msw";
 
 export const mockToken = "mockToken";
-export const fakeUsername = "fakeUsername";
+export const fakeUsername = "Luis";
 
 export const handlers = [
   rest.post(`${process.env.REACT_APP_API_URL}user/register`, (req, res, ctx) =>
     res(ctx.status(201), ctx.json({ user: fakeUsername }))
+  ),
+
+  rest.post(`${process.env.REACT_APP_API_URL}user/login`, (req, res, ctx) =>
+    res(ctx.status(200), ctx.json({ user: fakeUsername }))
   ),
 ];
--- src/redux/thunks/userThunks.test.tsx
+++ src/redux/thunks/userThunks.test.tsx
@@ -1,18 +1,41 @@
 import server from "../../mocks/server";
-import { userRegisterThunk } from "./userThunks";
+import { userRegisterThunk, userLoginThunk } from "./userThunks";
 
 beforeAll(() => {
   server.listen();
 });
+afterEach(() => {
+  server.resetHandlers();
+});
+
 afterAll(() => {
   server.close();
 });
 
+jest.mock("jwt-decode", () =>
+  jest.fn().mockResolvedValue({ username: "Luis" })
+);
+
+const dispatch = jest.fn();
+
+describe("Given a userLoginThunk", () => {
+  describe("When it's invoked", () => {
+    test("Then the dispatch function is called", async () => {
+      const thunk = userLoginThunk({
+        username: "Luis",
+        password: "1234",
+      });
+
+      await thunk(dispatch());
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+
 describe("Given a userRegisterThunk", () => {
   describe("When it is invoked", () => {
-    test("The dispatch function is called", async () => {
-      const dispatch = jest.fn();
-
+    test("Then the dispatch function is called", async () => {
       const thunk = userRegisterThunk({
         name: "Luis",
         username: "Luis",
