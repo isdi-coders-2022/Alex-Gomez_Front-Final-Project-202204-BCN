d14215eb2e496216681e0812206f98c00ef62cd9
Alejandro Gomez <jandrogo@gmail.com>
2022-06-07 19:07:23 +0200

    Added some style to Navbar component

--- src/components/NavBar/NavBarStyled.tsx
+++ src/components/NavBar/NavBarStyled.tsx
@@ -3,8 +3,15 @@ import styled from "styled-components";
 const NavBarStyled = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
   justify-content: space-around;
   margin-top: 1rem;
+  margin-bottom: 0;
+  height: 5rem;
+  background-color: rgb(23, 100, 192);
+  .logoutButton {
+    height: 3rem;
+  }
 `;
 
 export default NavBarStyled;
