036fbe9906db6db84f884d8b8921adb8cc262762
Alejandro Gomez <jandrogo@gmail.com>
2022-06-08 10:24:35 +0200

    Created MessageCreateForm, initial form without logic

---
+++ src/components/MessageCreateForm/MessageCreateForm.tsx
@@ -0,0 +1,12 @@
+import MessageCreateFormStyled from "./MessageCreateFormStyled";
+
+const MessageCreateForm = () => {
+  //image, category, text, *author, *likes
+  return (
+    <MessageCreateFormStyled>
+      <form></form>
+    </MessageCreateFormStyled>
+  );
+};
+
+export default MessageCreateForm;
---
+++ src/components/MessageCreateForm/MessageCreateFormStyled.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const MessageCreateFormStyled = styled.section``;
+
+export default MessageCreateFormStyled;
