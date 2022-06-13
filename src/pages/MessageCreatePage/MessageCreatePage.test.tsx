3f6ed946087316e2616457a7da4389a66a981a09
Alejandro Gomez <jandrogo@gmail.com>
2022-06-13 12:13:45 +0200

    Modified MessageCreatePage and it's test to work right

--- src/pages/MessageCreatePage/MessageCreatePage.test.tsx
+++ src/pages/MessageCreatePage/MessageCreatePage.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { mockCreateData } from "../../mocks/handlers";
 import store from "../../redux/store/store";
 import MessageCreatePage from "./MessageCreatePage";
 
