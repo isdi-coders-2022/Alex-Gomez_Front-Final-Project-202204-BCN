3cc313092eddc58757de6f841da9a2f6c48dfc0e
Alejandro Gomez <jandrogo@gmail.com>
2022-06-14 13:32:43 +0200

    Modified and added style to MessageList

--- src/components/MessageList/MessageListStyled.tsx
+++ src/components/MessageList/MessageListStyled.tsx
@@ -1,18 +1,34 @@
 import styled from "styled-components";
 
-const MessageListStyled = styled.ul`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  padding-left: 0;
-  list-style-type: none;
-  margin: 1rem;
-
-  @media (min-width: 400px) {
+const MessageListStyled = styled.div`
+  ul {
     display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 0;
+    list-style-type: none;
+    margin: 1rem;
+  }
+
+  .page-buttons {
+    background-color: rgb(23, 100, 192);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 3rem;
+  }
+
+  .button {
+    color: white;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+  @media (min-width: 400px) {
+    ul {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
   }
 `;
 
