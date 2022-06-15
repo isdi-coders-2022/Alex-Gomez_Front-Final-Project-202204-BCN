1421cd5594ad73883498d3960d1e97d807acb75a
Alejandro Gomez <jandrogo@gmail.com>
2022-06-15 19:22:52 +0200

    Added snapshot test to NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.test.tsx
+++ src/pages/NotFoundPage/NotFoundPage.test.tsx
@@ -3,8 +3,9 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store/store";
 import NotFoundPage from "./NotFoundPage";
+import TestRenderer from "react-test-renderer";
 
-describe("Given a LoginPage component", () => {
+describe("Given not notFoundPage", () => {
   describe("When it is invoked", () => {
     test("Then it should render a form with 2 inputs and a button", () => {
       render(
@@ -19,6 +20,17 @@ describe("Given a LoginPage component", () => {
       expect(
         screen.getByText("Go back or just type again the url")
       ).toBeInTheDocument();
+    });
+
+    test("Then it should match this snapshot", () => {
+      const notFoundPageRender = TestRenderer.create(
+        <BrowserRouter>
+          <Provider store={store}>
+            <NotFoundPage />
+          </Provider>
+        </BrowserRouter>
+      );
+      expect(notFoundPageRender).toMatchSnapshot();
     });
   });
 });
